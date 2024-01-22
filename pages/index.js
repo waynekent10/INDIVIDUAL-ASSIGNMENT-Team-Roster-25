c9d644f59e400291744511bfd5e1c539f46800c1
Bruce Clark <Bruce.clark55@gmail.com>
2024-01-22 01:17:35 -0500

    added index

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,34 @@
 import { Button } from 'react-bootstrap';
-import { signOut } from '../utils/auth';
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
 import { useAuth } from '../utils/context/authContext';
+import { getMembers } from '../api/fantasyData';
+import MemberCard from '../components/MemberCard';
 
 function Home() {
+  const [members, setMembers] = useState([]);
   const { user } = useAuth();
 
+  const getAllTheMembers = () => {
+    getMembers(user.uid).then(setMembers);
+  };
+
+  useEffect(() => {
+    getAllTheMembers();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user]);
+
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    >
-      <h1>Hello {user.displayName}! </h1>
-      <p>Click the button below to logout!</p>
-      <Button variant="danger" type="button" size="lg" className="copy-btn" onClick={signOut}>
-        Sign Out
-      </Button>
+    <div className="text-center my-4">
+      <Link href="/team/new" passHref>
+        <Button>Add A Member</Button>
+      </Link>
+      <div className="d-flex flex-wrap">
+        {members.map((member) => (
+          <MemberCard key={member.firebaseKey} memberObj={member} onUpdate={getAllTheMembers} />
+        ))}
+      </div>
+
     </div>
   );
 }
