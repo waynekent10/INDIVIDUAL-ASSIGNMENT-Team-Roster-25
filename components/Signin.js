b7676785654b4adca0835df408f7ac2660fa263c
Bruce Clark <Bruce.clark55@gmail.com>
2024-01-22 06:25:29 -0500

    changed the signin button color

--- components/Signin.js
+++ components/Signin.js
@@ -13,9 +13,9 @@ function Signin() {
         margin: '0 auto',
       }}
     >
-      <h1>Hi there!</h1>
-      <p>Click the button below to login!</p>
-      <Button type="button" size="lg" className="copy-btn" onClick={signIn}>
+      <h1>What it do!?!</h1>
+      <p>Click the button below to view your Team</p>
+      <Button type="button" size="lg" className="secondary" onClick={signIn}>
         Sign In
       </Button>
     </div>
